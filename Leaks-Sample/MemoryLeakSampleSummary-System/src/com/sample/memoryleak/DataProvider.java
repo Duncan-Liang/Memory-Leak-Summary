286084d303a2a376141f3b719cbbab348c4da275
Duncan-Liang <326219589@qq.com>
2016-09-08 17:29:55 +0800

    系统源码编译包，添加了对应泄漏检测包

---
+++ Leaks-Sample/MemoryLeakSampleSummary-System/src/com/sample/memoryleak/DataProvider.java
@@ -0,0 +1,27 @@
+package com.sample.memoryleak;
+
+/**
+ * Created by liangfeng on 2016/9/8.
+ */
+public class DataProvider {
+    public static int sCounter;
+    public static int getImageResId(){
+        int index = (sCounter++) % 5;
+        int resId = R.drawable.large1;
+        switch (index) {
+            case 0:
+                resId = R.drawable.img3;
+                break;
+            case 1:
+                resId = R.drawable.large2;
+                break;
+            case 2:
+                resId = R.drawable.large3;
+                break;
+            case 3:
+                resId = R.drawable.large4;
+                break;
+        }
+        return resId;
+    }
+}
---
+++ Leaks-Sample/MemoryLeakSampleSummary-System/src/com/sample/memoryleak/SummaryApplication.java
@@ -0,0 +1,26 @@
+package com.sample.memoryleak;
+import android.app.Application;
+import android.content.Context;
+import com.squareup.leakcanary.LeakCanary;
+import android.os.StrictMode;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.GINGERBREAD;
+
+public class SummaryApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        enabledStrictMode();
+        LeakCanary.install(this);
+    }
+
+    private void enabledStrictMode() {
+        if (SDK_INT >= GINGERBREAD) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                    .detectAll() //
+                    .penaltyLog() //
+                    .penaltyDeath() //
+                    .build());
+        }
+    }
+}
